643129c8e4b797c48258b05d0b5e85eae197454b
alac8559 <alac8559@colorado.edu>
2020-03-08 01:56:53 -0700

    First draft of registration page

---
+++ Code/registration/translate.js
@@ -0,0 +1,16 @@
+function translateMe(){
+  document.title = "Registration";
+  document.getElementById("titleH1").innerHTML = "Create Account";
+  document.getElementById("fullNameTrans").innerHTML = "Full Name";
+  document.getElementById("fullName").placeholder = "Enter Name";
+  document.getElementById("emailTrans").innerHTML = "Email Address";
+  document.getElementById("emailAddress").placeholder = "Enter Email";
+  document.getElementById("passTrans").innerHTML = "Password";
+  document.getElementById("passwordFirst").placeholder = "Password";
+  document.getElementById("passConfirmTrans").innerHTML = "Confirm Password";
+  document.getElementById("passwordConfirm").placeholder = "Confirm Password";
+  document.getElementById("submitTrans").innerHTML = "Already have an account? ";
+  document.getElementById("signTrans").innerHTML = "Submit";
+  document.getElementById("hyperLinkTrans").innerHTML = "Login here";
+  alert("Soon enough you will understand what that gibberish was with the help of BeepBoop!");
+}
