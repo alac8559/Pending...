bd55bde3de68014ba3fec53ec95208d793158f96
alac8559 <alac8559@colorado.edu>
2020-04-22 00:01:17 -0600

    Login and Server.js

--- Code/registration/server.js
+++ Code/registration/server.js
@@ -1,15 +1,40 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+var path = require('path')
+
+const initializePassport = require('./passport-config')
+initializePassport(passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
 
 const users = []
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
 
-app.get('/', (req,res) =>{
-    res.render('index.ejs', {name: 'Kyle'})
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/home', (req,res) => {
+//    res.sendFile(path.join(__dirname+'/../Home/Homepage.html'))
+    res.render('Home/Homepage.ejs', {fullname: req.user.name})
 })
+
 app.get('/login', (req,res) =>{
     res.render('login.ejs')
 
@@ -37,6 +62,10 @@ app.post('/register', async (req,res) =>{
     console.log(users)
 })
 
-
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
 app.listen(3000)
