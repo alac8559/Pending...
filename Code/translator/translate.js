1dedf77be4437dfa714ced3a63b887c9ee3260cf
lumeeer <38306992+lumeeer@users.noreply.github.com>
2020-04-21 22:54:48 -0600

    Update
    Now checks the input and supports both upper and lower case letters.

--- Code/translator/translate.js
+++ Code/translator/translate.js
@@ -1,32 +1,32 @@
 var code = "";
 var sentence = "";
 var charCodes=new Array(36);
-charCodes["a"]=". _";
-charCodes["b"]="_ . . .";
-charCodes["c"]="_ . _ .";
-charCodes["d"]="_ . .";
-charCodes["e"]=".";
-charCodes["f"]=". . _ .";
-charCodes["g"]="_ _ .";
-charCodes["h"]=". . . .";
-charCodes["i"]=". .";
-charCodes["j"]=". _ _ _";
-charCodes["k"]="_ . _";
-charCodes["l"]=". _ . .";
-charCodes["m"]="_ _";
-charCodes["n"]="_ .";
-charCodes["o"]="_ _ _";
-charCodes["p"]=". _ _ .";
-charCodes["q"]="_ _ . _";
-charCodes["r"]=". _ .";
-charCodes["s"]=". . .";
-charCodes["t"]="_";
-charCodes["u"]=". . _";
-charCodes["v"]=". . . _";
-charCodes["w"]=". _ _";
-charCodes["x"]="_ . . _";
-charCodes["y"]="_ . _ _";
-charCodes["z"]="_ _ . .";
+charCodes["A"]=". _";
+charCodes["B"]="_ . . .";
+charCodes["C"]="_ . _ .";
+charCodes["D"]="_ . .";
+charCodes["E"]=".";
+charCodes["F"]=". . _ .";
+charCodes["G"]="_ _ .";
+charCodes["H"]=". . . .";
+charCodes["I"]=". .";
+charCodes["J"]=". _ _ _";
+charCodes["K"]="_ . _";
+charCodes["L"]=". _ . .";
+charCodes["M"]="_ _";
+charCodes["N"]="_ .";
+charCodes["O"]="_ _ _";
+charCodes["P"]=". _ _ .";
+charCodes["Q"]="_ _ . _";
+charCodes["R"]=". _ .";
+charCodes["S"]=". . .";
+charCodes["T"]="_";
+charCodes["U"]=". . _";
+charCodes["V"]=". . . _";
+charCodes["W"]=". _ _";
+charCodes["X"]="_ . . _";
+charCodes["Y"]="_ . _ _";
+charCodes["Z"]="_ _ . .";
 charCodes["1"]=". _ _ _ _";
 charCodes["2"]=". . _ _ _";
 charCodes["3"]=". . . _ _";
@@ -39,10 +39,40 @@ charCodes["9"]="_ _ _ _ .";
 charCodes["0"]="_ _ _ _ _";
 
 function l2c(){
-    sentence = document.getElementById("l").value;
+
+
+    this.sentence = document.getElementById("l").value;
+
+    for(var i = 0; i < this.sentence.length; i++)
+    {
+        var lt = this.sentence.charAt(i);
+
+        if(!lt.match(/[a-zA-Z0-9]/))
+        {
+            if(lt != " ")
+            {
+                alert("Please only enter letters and numbers.");
+                return;
+            }
+        }
+
+    }
+
+
+    filter(this.sentence);
+
     code = sentence;
     trans(sentence);
     document.getElementById("code").innerHTML = trans(sentence);
+}
+
+
+
+function filter(sent)
+{
+
+    this.sentence = this.sentence.toUpperCase();
+
 }
 
 function trans(letter)
@@ -61,26 +91,27 @@ function trans(letter)
 
 
 
-function c2a()
+function CreateAudioContext(freq)
 {
+
     var AudioContext = window.AudioContext || window.webkitAudioContext;
     this.beep = new AudioContext();
     this.oscillator = this.beep.createOscillator();
     this.gain = this.beep.createGain();
     this.time = this.beep.currentTime;
-
     this.gain.gain.value = 0;
-    this.oscillator.frequency.value = 750;
+    this.oscillator.frequency.value = freq;
     this.oscillator.connect(gain);
     this.gain.connect(this.beep.destination);
     this.oscillator.start(0);
 
+}
 
+function c2a()
+{
+
+    CreateAudioContext(750);
     var dur = 0.07;
-
-    //this.gain.gain.setValueAtTime(1.0, time);
-    //this.gain.gain.setValueAtTime(0, time + 0.5);
-
 
     for(var i = 0; i < code.length; i++)
     {
